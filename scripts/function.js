15be2b0827484093ce927b7b334f4c967f24ffd3
Rifat Rahman <rifat.rahman378@gmail.com>
2024-04-07 14:51:50 +0600

    'design'

--- scripts/function.js
+++ scripts/function.js
@@ -1,5 +1,18 @@
 function calculateTriangle() {
-    const base = document.getElementById('triangle-base');
-    const baseValue = parseFloat(base.value);
-    console.log(baseValue);
+    const base = inputValueById('triangle-base');
+    const height = inputValueById('triangle-height')
+    const area = '' + (base * height * 0.5);
+    setInnerText('triangle-id', area)
+}
+
+
+// function to get input
+function inputValueById(inputValue) {
+    const Value = parseFloat(document.getElementById(inputValue).value);
+    return Value;
+}
+// function to set text
+function setInnerText(elementId, area) {
+    const element = document.getElementById(elementId);
+    element.innerText = (area);
 }
